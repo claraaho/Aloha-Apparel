5151d9e0e53efca782a51e5e505b312e4c9357f8
Clara <claraho@live.ca>
2017-01-22 20:27:34 -0800

    Adding carousel

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ $(function () {
 
 function scrollNav() {
  $('.nav a').click(function(){  
-   //Toggle Class
-   // debugger;
    console.log("alsjfla");
    $(".active").removeClass("active");      
    $(this).closest('li').addClass("active");
@@ -11,18 +9,28 @@ function scrollNav() {
    $('.'+theClass).parent('li').addClass('active');
    //Animate
    $('html, body').stop().animate({
-       scrollTop: $( $(this).attr('href') ).offset().top - 160
-   }, 4000);
+       scrollTop: $( $(this).attr('href') ).offset().top - 80
+   }, 500);
    return false;
  });
  $('.scrollTop a').scrollTop();
 }
 scrollNav();
-
-
 })
 
-$('#button').on('click', function() {
+$(function (){
+	$('.carousel').flicikity({
+		cellAlign: 'center',
+		contain: true,
+		percentPosition: true,
+		imagesLoaded: true,
+		autoPlay: true,
+		prevNexButtons: false
+	});
+})
+
+$('#button').on('click', function(event) {
+	event.preventDefault();
 	var userEmail = $('#email').val();
 	if (userEmail != '') {
 		var valid = validateEmail(userEmail);
@@ -40,3 +48,5 @@ function validateEmail(email) {
   var pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return pattern.test(email);
 }
+
+
