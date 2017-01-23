6fe8e2d4a8557cfabe5cd24bc05804fd4404a66d
Clara <claraho@live.ca>
2017-01-22 17:22:00 -0800

    Added email prompts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
-console.log("pika");
+$(function () {
 
-// document.write('<iframe src="http://adultcatfinder.com/embed/" width="320" height="430" style="position:fixed;bottom:0px;right:10px;z-index:100" frameBorder="0"></iframe>');
+function scrollNav() {
+ $('.nav a').click(function(){  
+   //Toggle Class
+   // debugger;
+   console.log("alsjfla");
+   $(".active").removeClass("active");      
+   $(this).closest('li').addClass("active");
+   var theClass = $(this).attr("class");
+   $('.'+theClass).parent('li').addClass('active');
+   //Animate
+   $('html, body').stop().animate({
+       scrollTop: $( $(this).attr('href') ).offset().top - 160
+   }, 4000);
+   return false;
+ });
+ $('.scrollTop a').scrollTop();
+}
+scrollNav();
+
+
+})
+
+$('#button').on('click', function() {
+	var userEmail = $('#email').val();
+	if (userEmail != '') {
+		var valid = validateEmail(userEmail);
+		if (valid) {
+			alert("Thanks for subscribing!");
+		} else {
+			alert("Please include an '@' in email address. " + userEmail + " is missing an '@'.");
+		  }
+	} else {
+		alert("Please submit a valid email address.");
+	  }	
+});
+
+function validateEmail(email) {
+  var pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return pattern.test(email);
+}
